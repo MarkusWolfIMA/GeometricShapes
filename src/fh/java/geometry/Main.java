6666072a721b025d1e0912ba859048db84d4bf92
Markus Wolf <markus.wolf@koerbler.com>
2016-11-04 13:14:52 +0100

    Rejected commit: Default

--- src/fh/java/geometry/Main.java
+++ src/fh/java/geometry/Main.java
@@ -1,5 +1,6 @@
 package fh.java.geometry;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
 import fh.java.geometry.model.GeometricShape;
 import fh.java.geometry.model.curved.Circle;
 import fh.java.geometry.model.quadrilateral.Rectangle;
@@ -12,9 +13,27 @@ import java.util.stream.Collectors;
 public class Main {
 		//TODO: main() is missing
 
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.doIt();
+    }
+
+    public void doIt(){
+        List<GeometricShape> shapeList = new ArrayList<>();
+        fillList(shapeList);
+        printList(shapeList);
+
+
+    }
 
     public void fillList(List<GeometricShape> shapeList) {
-		//TODO: Implement
+
+        shapeList.add(new Circle(0,0,2.5));
+        shapeList.add(new Circle(1,1,3));
+        shapeList.add(new Circle(2,2,4));
+
+        shapeList.add(new Rectangle(0,0,4.0,6.0));
+
 
     }
 
@@ -22,6 +41,21 @@ public class Main {
     public void printList(List<GeometricShape> shapeList) {
 		//TODO: Implement
 
+        System.out.println("---------------FOR LOOP-----------------");
+        for(int pos = 0; pos<shapeList.size();pos++){
+            System.out.println(shapeList.get(pos));
+        }
+
+        System.out.println("-----------------FOR EACH---------------");
+        for (GeometricShape shape : shapeList) {
+            System.out.println(shape);
+        }
+
+        System.out.println("---------------STREAM 1---------------- ");
+        shapeList.stream().forEach(shape -> System.out.println(shape));
+
+        System.out.println("---------------STREAM 2-----------------");
+        shapeList.stream().forEach(System.out ::println);
     }
 
 }
